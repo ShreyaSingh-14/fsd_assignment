027f61ec994415f022671017f97c8dea6a62a7d2
Shreya Singh <ashfame14@gmail.com>
2025-11-20 15:09:45 +0530

    Add files via upload

---
+++ controllers/authController.js
@@ -0,0 +1,14 @@
+const USER = {
+    email: "test@example.com",
+    password: "12345"
+};
+
+exports.loginUser = (req, res) => {
+    const { email, password } = req.body;
+
+    if (email === USER.email && password === USER.password) {
+        return res.json({ message: "Login Successful" });
+    }
+
+    return res.status(401).json({ message: "Invalid Credentials" });
+};
---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const authRoutes = require("./routes/authRoutes");
+
+app.use(express.json());
+
+app.use("/auth", authRoutes);
+
+app.listen(5000, () => {
+    console.log("Server running on port 5000");
+});
